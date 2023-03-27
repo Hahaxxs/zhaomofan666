1bdea48ee6a1a9c70909528dbf174ac06d7c0762
Hahaxxs <2900127202@qq.com>
2023-03-27 13:58:46 +0800

    first commit

---
+++ app/src/main/java/com/mofan/zhaomofan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mofan.zhaomofan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.callback.ApiResponse
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
